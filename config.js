ede151ffa1438df8d368407edb519762fbe5f7e7
SLNUWA <thilinuwan56@gmail.com>
2024-05-16 22:09:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741522452', 'me', true],
+  ['941522452', 'nuwan', true], 
   [''] 
 ] //Number of owners
 
